f0d8c8312377f05d02d2f0b665850becc3ccbe48
develoh973 <68682578+develoh973@users.noreply.github.com>
2020-09-09 20:12:29 +0900

    HandUnivertyTitleUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('HandUniversity'),
       ),
       body: Center(
         child: Column(
