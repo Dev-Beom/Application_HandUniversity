8977161d0ac06ad771d6744a5af57a4c40bfa3ac
changseok <develoh973@gmail.com>
2020-09-19 15:50:53 +0900

    Swiper_fraction 수정 필요

---
+++ lib/swiper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class Swiperpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.asset('assets/1.png');
+        },
+        indicatorLayout: PageIndicatorLayout.NONE,
+        autoplay: false,
+        itemCount: 3,
+        pagination: new SwiperPagination(builder: SwiperPagination.fraction),
+        control: new SwiperControl(iconNext: null, iconPrevious: null),
+      ),
+    );
+  }
+}
